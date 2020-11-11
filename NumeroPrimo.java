64435579c26608cdb73631cfcc2f7108571a235e
Justomed <57358099+Justomed@users.noreply.github.com>
2020-11-11 09:04:03 -0300

    Update NumeroPrimo.java

--- NumeroPrimo.java
+++ NumeroPrimo.java
@@ -21,11 +21,11 @@ public class numeroPrimo
         }else{
             System.out.println("El numero no es primo");
         }
-        /* a) agrego para saber si el numero es par o impar*/
+        /*agrego para saber si el numero es par o impar
             if (numero%2==0){
                System.out.println("El número es par");
              else
                System.out.println("El número es impar");
-            }
+            }*/
     }
 }
